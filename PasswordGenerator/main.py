fd2831540933c1b6eef8ee7fa2641b57b86cbcc2
Skye Vitiritti <skylar.vitiritti@Fiserv.com>
2023-04-07 12:46:18 -0400

    Added copy feature. Added better on-screen messages.

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,37 +1,43 @@
 #import libraries
-from msilib.schema import CheckBox
 import random
+import tkinter.messagebox
 from tkinter import *
 import string
-import tkinter
+import tkinter as tk
+import pyperclip as pc
 
 #create UI
 window = Tk()
+canvas1 = tk.Canvas(window, width=500, height=300, relief='flat')
+canvas1.pack()
+
 window.title('Password Generator')
-window.geometry('500x500')
 
-Label (window, font=('bold', 10), text='PASSWORD GENERATOR').pack()
+label1 = tk.Label (window, font=('bold', 10), text='PASSWORD GENERATOR')
+canvas1.create_window(250,25, window=label1)
 
-#define functions
+label2 = tk.Label (window, text='Enter Password Length (8 to 32 Characters)', font=('normal', 10))
+canvas1.create_window(250,100, window=label2)
+
+entry1 = tk.Entry(window)
+canvas1.create_window(250, 140, window=entry1)
+
+#define password
 def password_generate(leng):
     valid_char='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@_$%'
-    password=''.join(random.sample(valid_char, leng))
-    l = Label(window, text= password, font=('bold', 20))
-    l.place(x=90, y=50)
+    password = ''.join(random.sample(valid_char, leng))
+    pc.copy(password)
+    Label (window, text="Password Automatically Copied To Clipboard", fg='green', font=('bold', 20)).pack()
 
 #determine password
 def get_len():
+    password_len = int(entry1.get())
     if (32 >= password_len >= 8):
         password_generate(password_len)
     else:
-        print("Invalid input. Ending application.")
-        exit()
-
-
-#password length variable
-password_len = int (input("Enter password length between 8 and 32 characters: "))
+        Label(window, text='Error: Please try again.', font=('bold', 20), fg='red').place(x=120, y=200)
 
 #generate Generate button
-Button (window, text='Generate Password', font=('normal', 10), bg='yellow', command=get_len).place(x=200,y=100)
+Button (window, text='Generate Password', font=('normal', 10), bg='yellow', command=get_len).pack()
 
 window.mainloop()
